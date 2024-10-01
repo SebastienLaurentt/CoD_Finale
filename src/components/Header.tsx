0fc5b6b9990b6e67822bde1fb8e45d760d936099
SebastienLaurentt <slaurent.26@gmail.com>
2024-10-02 01:33:35 +0200

    use useLocalion to update header span according url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,29 @@
+import { useLocation } from "react-router-dom";
 import SwitchOnLogo from "../assets/SwitchOnLogo.svg";
 import FinaleLogo from "./FinalLogo";
 
 const Header = () => {
+  const location = useLocation();
+
+  const getHeaderText = () => {
+    switch (location.pathname) {
+      case "/":
+        return "CLASH TIME!";
+      case "/pick":
+        return "PICK A CHAMPION !";
+      case "/fight":
+        return "3, 2, 1 FIGHT !";
+      default:
+        return "CLASH OF DEV";
+    }
+  };
+
   return (
     <header className="w-full pb-8 pt-[40px] 2xl:pt-5">
       <div className="relative mx-auto flex w-[366px] flex-row items-center justify-between 2xl:w-[1376px]">
         <FinaleLogo />
         <span className="absolute w-full text-center text-xs font-bold text-foreground">
-          CLASH TIME!
+          {getHeaderText()}
         </span>
         <img src={SwitchOnLogo} alt="Logo de Clash of Dev dans un toogle" />
       </div>
