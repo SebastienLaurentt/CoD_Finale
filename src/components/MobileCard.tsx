473eee7ce30b0b31ae07b88917723b55ff879bc7
Sebastien Laurent <slaurent.26@gmail.com>
2024-10-04 12:19:26 +0200

    add aria label and alt to improve accessibility

--- src/components/MobileCard.tsx
+++ src/components/MobileCard.tsx
@@ -8,7 +8,7 @@ const MobileCard = () => {
       <p className="mt-[102px] text-justify text-[40px] font-semibold leading-[48px]">
         Oops ! Le gameplay est réservé aux joueurs PC (ehoui)...{" "}
       </p>
-      <LinkButton to="/pick" className="mx-auto mt-[97px]" variant="secondary">
+      <LinkButton to="/pick" className="mx-auto mt-[97px]" ariaLabel="Cliquer ici pour commencer la partie" variant="secondary">
         Start the Game
       </LinkButton>
       <img
