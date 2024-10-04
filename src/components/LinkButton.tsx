d280194f6097871f3d990831952fb7c04d98b4d6
Sebastien Laurent <slaurent.26@gmail.com>
2024-10-04 17:00:38 +0200

    add press effect

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,18 +1,18 @@
-import React, { forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
+import React, { forwardRef } from "react";
 import { Link } from "react-router-dom";
 
 const buttonVariants = cva(
-  "w-[212px] rounded-[60px] px-2.5 py-[22px] text-center text-sm font-semibold",
-  {
+  "w-[212px] rounded-[60px] px-2.5 py-[22px] text-center text-sm font-semibold transition-transform",
+  { 
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground",
+        default: "bg-primary text-primary-foreground ",
         secondary: "bg-background text-foreground",
       },
       disabled: {
         true: "pointer-events-none cursor-not-allowed opacity-60",
-        false: "cursor-pointer",
+        false: "cursor-pointer hover:scale-[0.98] active:scale-[0.96]",
       },
     },
     defaultVariants: {
@@ -27,34 +27,31 @@ interface LinkButtonProps extends VariantProps<typeof buttonVariants> {
   children: React.ReactNode;
   className?: string;
   disabled?: boolean;
-  ariaLabel: string; 
+  ariaLabel: string;
 }
 
-const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(({
-  to,
-  children,
-  variant,
-  className = "",
-  disabled = false,
-  ariaLabel,
-}, ref) => {
-  return (
-    <Link
-      ref={ref}
-      to={to}
-      role="button"
-      className={buttonVariants({ variant, disabled, className })}
-      onClick={(e) => {
-        if (disabled) {
-          e.preventDefault();
-        }
-      }}
-      aria-label={ariaLabel} 
-    >
-      {children}
-    </Link>
-  );
-});
-
+const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(
+  (
+    { to, children, variant, className = "", disabled = false, ariaLabel },
+    ref
+  ) => {
+    return (
+      <Link
+        ref={ref}
+        to={to}
+        role="button"
+        className={buttonVariants({ variant, disabled, className })}
+        onClick={(e) => {
+          if (disabled) {
+            e.preventDefault();
+          }
+        }}
+        aria-label={ariaLabel}
+      >
+        {children}
+      </Link>
+    );
+  }
+);
 
 export default LinkButton;
